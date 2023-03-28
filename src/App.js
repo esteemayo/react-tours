c06d757669e7eb948d74bdd75725438e6f14bb81
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-28 01:17:36 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
-import Loading from "./components/Loading";
-import Tours from "./components/Tours";
+import Loading from './components/Loading';
+import Tours from './components/Tours';
 
 import "./App.css";
 
