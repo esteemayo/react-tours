adf82d224f8ff9ef93f6298815c7c3245cf88706
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-28 01:25:12 +0100

    fix: modify

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -7,7 +7,7 @@ const Tour = ({ id, name, info, image, price, onDelete }) => {
     <article className='single-tour'>
       <img src={image} alt={name} />
       <footer>
-        <div className="tour-info">
+        <div className='tour-info'>
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
