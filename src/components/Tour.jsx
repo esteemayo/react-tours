3da0fab6d8eb04a69092c3c0ba7dff0a4fabe93b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-28 01:25:21 +0100

    fix: modify

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -9,7 +9,7 @@ const Tour = ({ id, name, info, image, price, onDelete }) => {
       <footer>
         <div className='tour-info'>
           <h4>{name}</h4>
-          <h4 className="tour-price">${price}</h4>
+          <h4 className='tour-price'>${price}</h4>
         </div>
         <p>
           {readMore ? info : info.substr(0, 200)}...
